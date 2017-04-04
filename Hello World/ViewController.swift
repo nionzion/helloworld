35f86b910e359c5084b9da35691ba9f02f1d7eef
Daniel Neer <neer.daniel@gmail.com>
2017-04-04 14:23:21 +0200

    Added interaction with buttons and labels

--- Hello World/ViewController.swift
+++ Hello World/ViewController.swift
@@ -6,13 +6,24 @@
 //  Copyright © 2017 prettyfl4cko. All rights reserved.
 //
 
+
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var textFieldName: UITextField!
+    @IBOutlet weak var labelNameResult: UILabel!
+    @IBAction func buttonSubmit(_ sender: Any) {
+        //Clicking on Submit changes labelNameResult to textFieldContents
+        labelNameResult.text = "Hi, " + textFieldName.text!
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
